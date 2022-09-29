21f42d9390ff231870957e9e202c835f203f60ff
Ogushanakin <ogzhnakn96@gmail.com>
2022-09-29 22:06:02 +0300

    first commit

---
+++ ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by AKIN on 29.09.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
